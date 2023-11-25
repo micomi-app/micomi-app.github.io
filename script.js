0edb0c40a1ea5517da56f0c61c6b5beb8403729a
1step621 <onestep621@gmail.com>
2023-11-25 20:50:50 +0900

    Add fonts and iroiro

--- script.js
+++ script.js
@@ -2,7 +2,5 @@ window.addEventListener('DOMContentLoaded', () => {
   new LocomotiveScroll({
     el: document.querySelector('[data-scroll-container]'),
     smooth: true,
-    tablet: { smooth: true },
-    smartphone: { smooth: true }
   });
 });
