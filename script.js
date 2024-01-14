ff68f19807087ea4e422215a7439cc6be8ff2ac3
1Step621 <onestep621@gmail.com>
2024-01-14 12:52:00 +0900

    iroiro

--- script.js
+++ script.js
@@ -3,4 +3,12 @@ window.addEventListener('DOMContentLoaded', () => {
     el: document.querySelector('[data-scroll-container]'),
     smooth: true,
   });
+  bubbly({
+    colorStart: '#fff',
+    colorStop: '#fff',
+    compose: 'source-over',
+    bubbles: 10,
+    bubbleFunc: () => `#ff9f0050`,
+    velocityFunc: () => 0.4,
+  });
 });
